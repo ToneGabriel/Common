32dabdbab752c2468b6f37e5ff473e2c1e0cb37f
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2024-06-11 23:03:50 +0300

    Update

--- code/helpers/ThreadPool.h
+++ code/helpers/ThreadPool.h
@@ -5,8 +5,26 @@
 #include <condition_variable>
 #include <atomic>
 #include <queue>
+#include <string>
 #include <functional>
 #include "Common.h"
+
+
+template<class Container, class = void>
+struct _HasValidMethods : std::false_type {};
+
+template<class Container>
+struct _HasValidMethods<Container, std::void_t< decltype(std::declval<Container>().size()),
+                                                decltype(std::declval<Container>().begin()),
+                                                decltype(std::declval<Container>().end())>> : std::true_type {};
+
+template<class Container>
+using _EnableJobsFromContainer_t =
+            std::enable_if_t<std::conjunction_v<
+                                    std::is_same<typename Container::value_type, std::function<void(void)>>,
+                                    std::is_convertible<typename std::iterator_traits<typename Container::iterator>::iterator_category, std::forward_iterator_tag>,
+                                    _HasValidMethods<Container>>,
+            bool>;
 
 
 template<size_t NThread = 2>
@@ -14,31 +32,16 @@ class ThreadPool
 {
 private:
     static_assert(NThread < MACHINE_NUM_PROCESSORS, "Number of threads exceeds the system capability!");
-
-    template<class Container, class = void>
-    struct _HasValidMethods : std::false_type {};
-
-    template<class Container>
-    struct _HasValidMethods<Container, std::void_t< decltype(std::declval<Container>().clear()),
-                                                    decltype(std::declval<Container>().size()),
-                                                    decltype(std::declval<Container>().begin()),
-                                                    decltype(std::declval<Container>().end())>> : std::true_type {};
-
-    template<class Container>
-    using _EnableThreadPoolFromContainer_t =
-                std::enable_if_t<std::conjunction_v<
-                                        std::is_same<typename Container::value_type, std::function<void(void)>>,
-                                        std::is_convertible<typename std::iterator_traits<typename Container::iterator>::iterator_category, std::forward_iterator_tag>,
-                                        _HasValidMethods<Container>>,
-                bool>;
-
-private:
+    
     std::queue<std::function<void(void)>>   _jobs;
     std::thread                             _workerThreads[NThread];
     std::mutex                              _poolMtx;
     std::condition_variable                 _poolCV;
     std::atomic_size_t                      _jobsDone;
     bool                                    _shutdown;
+
+    std::queue<std::string>                 _errors;
+    std::mutex                              _errorMtx;
 
 public:
     // Constructors & Operators
@@ -68,7 +71,7 @@ public:
         _poolCV.notify_one();
     }
 
-    template<class JobContainer, _EnableThreadPoolFromContainer_t<JobContainer> = true>
+    template<class JobContainer, _EnableJobsFromContainer_t<JobContainer> = true>
     void do_more_jobs(JobContainer&& newJobs)
     {
         // Move multiple jobs on the queue and unblock necessary threads
@@ -79,9 +82,8 @@ public:
         const size_t addedSize = newJobs.size();
 
         // Move jobs from container and clear it
-        for (auto&& val : newJobs)
-            _jobs.emplace(std::move(val));
-        newJobs.clear();
+        for (auto&& job : newJobs)
+            _jobs.emplace(std::move(job));
 
         // Notify waiting threads
         for (size_t i = 0; i < addedSize; ++i)
@@ -103,14 +105,12 @@ private:
 
         // Create the specified number of threads
         for (auto& t : _workerThreads)
-            t = std::thread(std::bind(&ThreadPool<NThread>::_worker_thread, this));
+            t = std::thread(std::bind(&ThreadPool::_worker_thread, this));
     }
 
     void _close_pool()
     {
         // Notify all threads to stop waiting for job
-        // Threads will exit the loop and will join here
-
         {   // empty scope start -> lock
             std::unique_lock<std::mutex> lock(_poolMtx);
 
@@ -118,8 +118,16 @@ private:
             _poolCV.notify_all();
         }   // empty scope end -> unlock
 
+        // Threads will exit the loop and will join here
         for (auto& t : _workerThreads)
             t.join();
+
+        // Print errors last
+        while(!_errors.empty())
+        {
+            std::cout << "Error: " << _errors.front() << '\n';
+            _errors.pop();
+        }
     }
 
     void _worker_thread()
@@ -152,11 +160,13 @@ private:
             }
             catch (const std::exception& e)
             {
-                std::cerr << "Job threw an exception: " << e.what() << '\n';
+                std::unique_lock<std::mutex> lock(_errorMtx);
+                _errors.emplace(e.what());
             }
             catch (...)
             {
-                std::cerr << "Job threw an unknown exception." << '\n';
+                std::unique_lock<std::mutex> lock(_errorMtx);
+                _errors.emplace("Job threw an unknown exception.");
             }
 
             // Count work done
@@ -166,10 +176,10 @@ private:
 
 public:
 
-    template<class JobContainer, _EnableThreadPoolFromContainer_t<JobContainer> = true>
+    template<class JobContainer, _EnableJobsFromContainer_t<JobContainer> = true>
     static void start_and_display(JobContainer&& newJobs)
     {
-        static const size_t _BAR_WIDTH = 50;
+        const size_t barWidth = 50;
 
         size_t total    = newJobs.size();
         size_t current  = 0;
@@ -180,33 +190,27 @@ public:
             ThreadPool<NThread> pool;
             pool.do_more_jobs(std::move(newJobs));
 
+            // Start Display
             while (current < total)
             {
                 current = pool.jobs_done();
                 
-                // Draw Progress
                 progress = static_cast<float>(current) / static_cast<float>(total);
-                position = static_cast<size_t>(_BAR_WIDTH * progress);
+                position = static_cast<size_t>(barWidth * progress);
 
                 std::cout << "[";
-                for (size_t i = 0; i < _BAR_WIDTH; ++i)
-                {
-                    if (i < position)
-                        std::cout << "=";
-                    else if (i == position)
-                        std::cout << ">";
-                    else
-                        std::cout << " ";
-                }
+                for (size_t i = 0; i < barWidth; ++i)
+                    if (i < position) std::cout << "=";
+                    else if (i == position) std::cout << ">";
+                    else std::cout << " ";
                 std::cout << "] " << static_cast<size_t>(progress * 100.0f) << " %\r";
-                std::cout.flush();
 
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
+
+            // End Display
+            std::cout << '\n';
         }   // empty scope end -> ThreadPool obj is destroyed, but waits for the threads to finish and join
 
-        // End Display
-        std::cout << '\n';
-        std::cout.flush();
     }
 };  // END ThreadPool
